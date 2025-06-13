af436f71c2c40b93714a0adcd8810d7b1062fed1
lavanya192007 <rorylavanya@gmail.com>
2025-06-13 19:24:35 +0530

    m

--- Mini_projects/ipl.c
+++ Mini_projects/ipl.c
@@ -6,17 +6,17 @@ int main()
     printf(" Enter your over now : ");
     scanf("%d", &o);
     printf("Enter your current score : ");
-    scanf("%d", &s);
+    scanf("%d\n", &s);
     ro=20-o;
-    printf(" Your remaining over = %d",ro);
+    printf(" Your remaining over = %d\n",ro);
     rr=s/o;
-    printf(" Your current run rate = %2f",rr);
+    printf(" Your current run rate = %2f\n",rr);
     p1=s+(ro*rr);
     p2=s+(ro*(ro+2));
     p3=s+(ro*(ro+5));
-    printf(" Your projected score = ",p1);
-    printf(" Your projected score = ",p2);
-    printf(" Your projected score = ",p3);
+    printf(" Your projected score = %d\n ",p1);
+    printf(" Your projected score =%d\n ",p2);
+    printf(" Your projected score =%d\n ",p3);
     return 0;
 
 }
