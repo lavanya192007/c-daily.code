2c06d77825a75edb5745a99ee096a5dc6c10b0a9
lavanya192007 <rorylavanya@gmail.com>
2025-06-16 17:12:54 +0530

    file created

---
+++ special_number/reverseno.c
@@ -0,0 +1,25 @@
+/*
+     Program        : theater Seat Booking
+     Author         : Lavanya.M
+     Date           : 16/6/2025
+     description    :This program help to reverse the given number.
+     */
+    #include<stdio.h>
+    int main()
+    {
+        int i,n;
+        printf(" Enter the number : ");
+        scanf("%d", &n);
+        while(n>0)
+        {
+            i=n%10;
+            printf(" %d", i);
+            n=n/10;
+        }
+        return 0;
+    }
+    /*
+    sample output :
+            Enter the number :  189
+            
+    */
